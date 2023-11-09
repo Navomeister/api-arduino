4a5e056ec71a29c7e5dcb54c143a958abe118a02
Navo <110341514+Navomeister@users.noreply.github.com>
2023-11-09 17:14:08 -0300

    atualizaçao string de conexao

--- database/conexao.php
+++ database/conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-    $servername = 'localhost';
-    $username = 'acessotech';
-    $password = 'acessotec';
+    $servername = 'doorsense-server.mysql.database.azure.com';
+    $username = 'breno';
+    $password = 'AcessoTech115';
     $dbname = 'doorsense';
 
     $conn = new mysqli($servername, $username, $password, $dbname);
