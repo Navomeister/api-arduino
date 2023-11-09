1d9bf6babc6755db715855445b31fa8de026359d
Navo <110341514+Navomeister@users.noreply.github.com>
2023-11-09 20:57:46 -0300

    teste 7

--- conexao.php
+++ conexao.php
@@ -1,10 +1,14 @@
 <?php
-
+    
     $servername = 'doorsense-server.mysql.database.azure.com';
     $username = 'breno';
     $password = 'AcessoTech115';
     $dbname = 'doorsense';
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    $conn = mysqli_init();
+    mysqli_ssl_set($conn, NULL, NULL, "/etc/ca-certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL); 
 
 ?>
